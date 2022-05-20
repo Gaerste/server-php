25109d8353b0693ccae156ba8aaae69ce4b95996
Alejandro Gaerste <alejandrogaerste411@gmail.com>
2022-05-20 21:20:50 +0200

    feature: relocate folder

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/style.css">
+    <title>PHP Environment</title>
+</head>
+
+<body>
+    <main class="container">
+        <form action="" class="form__container">
+            <input type="email" name="email" class="input" placeholder="Email">
+            <input type="password" name="password" class="input" placeholder="Password">
+            <button type="submit" class="btn"><span class="btn-txt">Send</span></button>
+        </form>
+    </main>
+</body>
+
+</html>
