a0841a096fe6ecda31306b5eedb9b4161987a28b
Alejandro Gaerste <alejandrogaerste411@gmail.com>
2022-05-20 21:39:52 +0200

    feature: add the folder

--- index.php
+++ index.php
@@ -5,13 +5,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./assets/style.css">
+    <link rel="stylesheet" href="../server-php/assets/style.css">
     <title>PHP Environment</title>
 </head>
 
 <body>
     <main class="container">
         <form action="" class="form__container">
+            <h2 class="form__title">Login</h2>
             <input type="email" name="email" class="input" placeholder="Email">
             <input type="password" name="password" class="input" placeholder="Password">
             <button type="submit" class="btn"><span class="btn-txt">Send</span></button>
